15a36bc367302c47bef75851022357053c8183f9
Shipra <code.shipra@gmail.com>
2017-02-20 11:40:57 +0530

    reformats code

--- src/forms.py
+++ src/forms.py
@@ -1,14 +1,11 @@
-from apscheduler.jobstores.base import JobLookupError
-from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timedelta
+
+import pytz
 from django import forms
-from django.conf import settings
 
-from src.logic import send_notification
+from src import notification_utils
 
-scheduler = BackgroundScheduler(settings.APSCHEDULER_SETTINGS)
-
-scheduler.start()
-
+utc = pytz.utc
 
 
 class NotificationForm(forms.Form):
@@ -18,14 +15,20 @@ class NotificationForm(forms.Form):
     header = forms.CharField(min_length=20, max_length=150)
     content = forms.CharField(min_length=20, max_length=300, widget=forms.Textarea)
     image_url = forms.URLField(widget=forms.URLInput)
-    notification_time = forms.DateTimeField(widget=forms.DateTimeInput)
+    notification_time = forms.DateTimeField(widget=forms.DateTimeInput, initial=datetime.utcnow())
+
+    # def clean_notification_time(self):
+    #     date = self.cleaned_data['notification_time']
+    #     print "tzinfo", date.tzinfo
+    #     one_minute_from_now = utc.localize(datetime.utcnow() + timedelta(minutes=1))
+    #     if date < one_minute_from_now:
+    #         raise forms.ValidationError("The notification_time must be 1 min more than current time")
+    #     return date
 
     def schedule_notification(self):
         time = self.cleaned_data.pop('notification_time')
         user_ids = self.cleaned_data.pop('query')
-        job_return = scheduler.add_job(send_notification, 'date', run_date=time,
-                                       kwargs={'user_ids': user_ids, 'payload': self.cleaned_data})
-        return job_return.id
+        return notification_utils.add_notification(payload=self.cleaned_data, user_ids=user_ids, time=time)
 
 
 class ModifyNotificationForm(forms.Form):
@@ -36,6 +39,7 @@ class ModifyNotificationForm(forms.Form):
         ('modify', 'Modify'),
         ('remove', 'Remove'),
     )
+
     action = forms.ChoiceField(choices=ACTIONS, widget=forms.RadioSelect)
     job_id = forms.CharField()
     query = forms.CharField(max_length=300, required=False)
@@ -44,31 +48,16 @@ class ModifyNotificationForm(forms.Form):
     image_url = forms.URLField(required=False, widget=forms.URLInput)
     notification_time = forms.DateTimeField(required=False, widget=forms.DateTimeInput)
 
-    @staticmethod
-    def reschedule_notification(job_id, notification_time, **kwargs):
-        job_return = scheduler.reschedule_job(job_id=job_id, jobstore='default', trigger='date',
-                                              run_date=notification_time)
-        return job_return.id, "rescheduled"
-
-    @staticmethod
-    def modify_notification(job_id, query, header, content, image_url, **kwargs):
-        payload = {
-            "header": header,
-            "content": content,
-            "image_url": image_url,
-        }
-        job_return = scheduler.modify_job(job_id=job_id, jobstore='default',
-                                          kwargs={'user_ids': query, 'payload': payload})
-        return job_return.id, "modified"
-
-    @staticmethod
-    def remove_notification(job_id, **kwargs):
-        scheduler.remove_job(job_id=job_id, jobstore='default')
-        return job_id, "removed"
+    # def clean_notification_time(self):
+    #     date = self.cleaned_data['notification_time']
+    #     one_minute_from_now = utc.localize(datetime.utcnow() + timedelta(minutes=1))
+    #     if date < one_minute_from_now:
+    #         raise forms.ValidationError("The notification_time must be 1 min more than current time")
+    #     return date
 
     def modify(self):
         action = self.cleaned_data.pop('action')
         job_id = self.cleaned_data.pop('job_id')
-        function = getattr(self, (action + '_notification'))
+        function = getattr(notification_utils, (action + '_notification'))
         job_id, action = function(job_id=job_id, **self.cleaned_data)
         return job_id, action
