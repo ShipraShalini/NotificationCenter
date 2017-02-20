6a02b6a9b977cc8b2e8771e256a8325a5ccebad9
Shipra <code.shipra@gmail.com>
2017-02-20 15:23:23 +0530

    modification in tests

--- src/tests.py
+++ src/tests.py
@@ -1,9 +1,16 @@
+#
+# Not complete, not tested.
+# use django-nose
+#
+
+
 from unittest import TestCase
 
 from datetime import datetime, timedelta
 from pytz import utc
 
 from src.forms import NotificationForm
+from src.notification_utils import add_notification
 
 
 class TestScheduleNotification(TestCase):
@@ -18,17 +25,89 @@ class TestScheduleNotification(TestCase):
         form = NotificationForm(data=form_data)
         self.assertTrue(form.is_valid())
 
-    # def
+
 
 class TestModifyNotification(TestCase):
-    def test_reschedule_form(self):
+
+    def add_notification_for_test(self):
+        payload = {
+            "header": "Test:This is header field data",
+            "content": "This is the content field data",
+            "notification_time": utc.localize(datetime.utcnow() + timedelta(minutes=2)),
+            "image_url": "https://image.flaticon.com/icons/png/128/118/118781.png"
+        }
+        return add_notification(payload=payload, user_ids='user_ids', time=utc.localize(datetime.utcnow() + timedelta(minutes=2)))
+
+    def test_reschedule_form_with_all_fields(self):
+        # job_id = self.add_notification_for_test()
         form_data = {
-            "job_id"
+            "job_id": "job_id",
+            "action": "reschedule",
             "query": 'user_ids',
             "header": "This is header field data",
             "content": "This is the content field data",
             "notification_time": utc.localize(datetime.utcnow() + timedelta(minutes=2)),
             "image_url": "https://image.flaticon.com/icons/png/128/118/118781.png"
+        }
+        form = NotificationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        
+    def test_modify_form_with_all_fields(self):
+        # job_id = self.add_notification_for_test()
+        form_data = {
+            "job_id": "job_id",
+            "action": "modify",
+            "query": 'user_ids',
+            "header": "This is header field data",
+            "content": "This is the content field data",
+            "notification_time": utc.localize(datetime.utcnow() + timedelta(minutes=2)),
+            "image_url": "https://image.flaticon.com/icons/png/128/118/118781.png"
+        }
+        form = NotificationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        
+    def test_delete_form_with_all_fields(self):
+        # job_id = self.add_notification_for_test()
+        form_data = {
+            "job_id": "job_id",
+            "action": "remove",
+            "query": 'user_ids',
+            "header": "This is header field data",
+            "content": "This is the content field data",
+            "notification_time": utc.localize(datetime.utcnow() + timedelta(minutes=2)),
+            "image_url": "https://image.flaticon.com/icons/png/128/118/118781.png"
+        }
+        form = NotificationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_reschedule_form(self):
+        # job_id = self.add_notification_for_test()
+        form_data = {
+            "job_id": "job_id",
+            "action": "reschedule",
+            "notification_time": utc.localize(datetime.utcnow() + timedelta(minutes=2)),
+        }
+        form = NotificationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_modify_form(self):
+        # job_id = self.add_notification_for_test()
+        form_data = {
+            "job_id": "job_id",
+            "action": "modify",
+            "query": 'user_ids',
+            "header": "This is header field data",
+            "content": "This is the content field data",
+            "image_url": "https://image.flaticon.com/icons/png/128/118/118781.png"
+        }
+        form = NotificationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_delete_form(self):
+        # job_id = self.add_notification_for_test()
+        form_data = {
+            "job_id": "job_id",
+            "action": "remove",
         }
         form = NotificationForm(data=form_data)
         self.assertTrue(form.is_valid())
